d02065cff10f5e418ac2633af2f4ac89e9eae0a0
yashLadha <201551061@iiitvadodara.ac.in>
2017-03-26 20:30:59 +0530

    Added Glide

--- app/src/main/java/own/code/play_nigga/NewsAdapter.java
+++ app/src/main/java/own/code/play_nigga/NewsAdapter.java
@@ -6,7 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -16,8 +19,11 @@ import java.util.ArrayList;
 
 public class NewsAdapter extends ArrayAdapter<NewsFeed> {
 
+    Context context;
+
     public NewsAdapter(Context context, int resource, ArrayList<NewsFeed> objects) {
         super(context, resource, objects);
+        this.context = context;
     }
 
     @NonNull
@@ -29,6 +35,9 @@ public class NewsAdapter extends ArrayAdapter<NewsFeed> {
         NewsFeed temp = getItem(position);
         TextView tvArticleName = (TextView) v.findViewById(R.id.tvArticleTopic);
         TextView tvArticleDescription = (TextView) v.findViewById(R.id.tvDescription);
+        ImageView imageView = (ImageView) v.findViewById(R.id.ivNewsIcon);
+
+        Glide.with(context).load(temp.getImage_url()).centerCrop().into(imageView);
 
         tvArticleDescription.setText(temp.getDescription());
         tvArticleName.setText(temp.getTitle());
